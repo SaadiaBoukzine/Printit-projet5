b4c51033759b3df24d99f8fc6c4cd267557359b8
SaadiaBoukzine <bsaadi21@gmail.com>
2023-07-11 20:16:46 +0200

    last commit

--- assets/script.js
+++ assets/script.js
@@ -29,14 +29,14 @@ imgbanniere.src = './assets/images/slideshow/'+slides[compteur].image;
 
 for(let i =0; i< slides.length;i++){
 	let selecpoint = document.createElement('div');
-	if(compteur == i){
+	if(compteur== i){
 		selecpoint.classList.add("dot_selected");
 	}
 	selecpoint.classList.add("dot");
 	points.appendChild(selecpoint);
 }
 
-let comptpoints = document.querySelector(".dot");
+let comptpoints = document.querySelectorAll(".dot");
 
 flecheDroite.addEventListener("click", ()=>{
 	if (compteur != slides.length-1){
@@ -47,26 +47,25 @@ flecheDroite.addEventListener("click", ()=>{
 		imgbanniere.src ='./assets/images/slideshow/'+slides[compteur].image;
 		tagLineslide.innerHTML = slides[compteur].tagLine;		
 		carousel(comptpoints);
-	console.log("ok");
+	
 });
 
 flecheGauche.addEventListener("click", ()=> {
 	if(compteur == 0){
 		compteur = slides.length;
-	}else{
-			compteur --;
 	}
+				compteur --;
 	imgbanniere.src ='./assets/images/slideshow/'+slides[compteur].image;
 	tagLineslide.innerHTML=slides[compteur].tagLine;
 	carousel(comptpoints);
-	console.log("ok");
+	
 });
 
 
 const carousel = (comptpoints) =>{
 	for(let i =0; i< comptpoints.length;i++){
 		comptpoints[i].classList.remove("dot_selected");	
-	if(cpt == i){
+	if(compteur == i){
 		comptpoints[i].classList.add("dot_selected");
 	}
 	comptpoints[i].classList.add("dot");
